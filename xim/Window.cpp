990c47fcc5c66a188a58bab53dd66b1c2f712003
Esben Christensen <71466904+blexaer@users.noreply.github.com>
2022-02-11 23:34:08 +0100

    Added Window abstraction, added test image, drawing of image, debug menu

---
+++ xim/Window.cpp
@@ -0,0 +1,42 @@
+#include "Window.h"
+
+//Namespace variables/Defines
+static HMENU hHelp;
+static HMENU hEdit;
+static HMENU hFile;
+static HMENU hTest;
+static HMENU hMenuBar;
+
+Window::Window(const char* title, int x, int y, int w, int h, Uint32 flags) {
+	window = SDL_CreateWindow(title, x, y, w, h, flags);
+}
+
+//Initializes the native windows drop down menu elements of the window
+void Window::ActivateMenu(HWND window_ref) {
+	hMenuBar = CreateMenu();
+	hFile = CreateMenu();
+	hEdit = CreateMenu();
+	hHelp = CreateMenu();
+	hTest = CreateMenu();
+	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hFile, L"File");
+	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hTest, L"Debug");
+	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hEdit, L"Edit");
+	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hHelp, L"Help");
+	AppendMenu(hFile, MF_STRING, ID_OPEN_FILE, L"Open");
+	AppendMenu(hFile, MF_STRING, ID_EXIT, L"Exit");
+	AppendMenu(hTest, MF_STRING, ID_DEBUG_PAINT, L"Repaint");
+	AppendMenu(hTest, MF_STRING, ID_DEBUG_FILL_BLACK, L"Fill black");
+	AppendMenu(hHelp, MF_STRING, ID_ABOUT, L"About");
+	SetMenu(window_ref, hMenuBar);
+}
+
+//Function which retrieves the address/Handle of an SDL window
+//Also retrieves the specific subsystem used by SDL to create that window which is platform specific (Windows, MAC OS x, IOS, etc...)
+HWND Window::GetSDLWinHandle(SDL_Window* win) {
+	SDL_SysWMinfo infoWindow;
+	SDL_VERSION(&infoWindow.version);
+	if (!SDL_GetWindowWMInfo(win, &infoWindow)) {
+		return NULL;
+	}
+	return (infoWindow.info.win.window);
+}
--- xim/xim.cpp
+++ xim/xim.cpp
@@ -1,22 +1,13 @@
-#include "SDL.h"
 #include <SDL_syswm.h>
 #include <stdio.h>
+#include <chrono>
+#include "SDL.h"
 #include "tinyfiledialogs.h"
 
-//Namespace variables/Defines
-#define ID_ABOUT 2
-#define ID_EXIT 3
-#define ID_CONTROLS 4
-#define ID_TEST 5
-static HMENU hHelp;
-static HMENU hEdit;
-static HMENU hFile;
-static HMENU hMenuBar;
+#include "window.h"
 
 HWND windowHandle;
-
-void ActivateMenu(HWND windowRef);
-HWND getSDLWinHandle(SDL_Window* win);
+SDL_Surface* image = NULL;
 
 int main(int argc, char* argv[]) {
 
@@ -27,54 +18,103 @@ int main(int argc, char* argv[]) {
 	else
 		SDL_Log("Initialized SDL2");
 
-	SDL_Window* window = NULL;
+	SDL_Window* sdlwindow = NULL;
 	SDL_Surface* screenSurface = NULL;
 
+#if defined(_WIN32)
+	//Enable WinAPI Events Processing
+	SDL_EventState(SDL_SYSWMEVENT, SDL_ENABLE);
+#endif // defined(_WIN32)
+
 	//Create window
-	window = SDL_CreateWindow("xim - cross image manipulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-	if (window != NULL)
-	{
-		screenSurface = SDL_GetWindowSurface(window);
+	Window window = Window("xim - cross image manipulator",
+							SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
+							SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+							sdlwindow = window.GetSDLWindow();
+	
+	if (sdlwindow) {
+		windowHandle = window.GetSDLWinHandle(sdlwindow);
+		window.ActivateMenu(windowHandle);
+
+		screenSurface = SDL_GetWindowSurface(sdlwindow);
 
 		//Fill the surface with white
 		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-		SDL_UpdateWindowSurface(window);
-		
+		SDL_UpdateWindowSurface(sdlwindow);
+
 		//SDL_Delay(2000);
 	}
 	else
-		SDL_Log("SDL_Window failed to be created! SDL_Error: %s\n", SDL_GetError());
+		SDL_LogError(1, "SDL_Window failed to be created! SDL_Error: %s\n", SDL_GetError());
 
-	//Enable WinAPI Events Processing
-	SDL_EventState(SDL_SYSWMEVENT, SDL_ENABLE);
-
-	windowHandle = getSDLWinHandle(window);
-	ActivateMenu(windowHandle);
+	image = SDL_LoadBMP("../res/tux.bmp");
+	if (!image) {
+		printf("Unable to load image %s! SDL Error: %s\n", "../res/tux.bmp", SDL_GetError());
+	}
 
 	SDL_Event mainEvent;
 
+	auto t1 = std::chrono::high_resolution_clock::now();
+	std::chrono::steady_clock::time_point t2;
+
 	bool isRunning = true;
 	while (isRunning) {
+		// Event handling
 		SDL_PollEvent(&mainEvent);
-		switch (mainEvent.type)
-		{
+		switch (mainEvent.type) {
 		case SDL_WINDOWEVENT_CLOSE:
 			mainEvent.type = SDL_QUIT;
 			SDL_PushEvent(&mainEvent);
 			break;
-		case SDL_SYSWMEVENT:
-			if (mainEvent.syswm.msg->msg.win.msg == WM_COMMAND)
-			{
-				if (LOWORD(mainEvent.syswm.msg->msg.win.wParam) == ID_EXIT)
-				{
-					isRunning = false;
-				}
-				else if (LOWORD(mainEvent.syswm.msg->msg.win.wParam) == ID_TEST)
-				{
-					tinyfd_openFileDialog("Select file", NULL, NULL, NULL, NULL, 0);
-				}
-			}
+		case SDL_WINDOWEVENT:
+			if (mainEvent.window.event == SDL_WINDOWEVENT_RESIZED) {
+				// Window SDL Surface is invalidated on resize, so we have to get the new one.
+				screenSurface = SDL_GetWindowSurface(sdlwindow);
+				SDL_Log("Debug: Resized and got new Window SDL Surface");
 
+				// Redraw workspace
+				SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+				SDL_UpdateWindowSurface(sdlwindow);
+			}
+			break;
+		case SDL_SYSWMEVENT:
+			switch (mainEvent.syswm.msg->msg.win.msg) {
+			case WM_COMMAND:
+				switch (LOWORD(mainEvent.syswm.msg->msg.win.wParam)) {
+				case Window::ID_EXIT:
+					isRunning = false;
+					break;
+				case Window::ID_OPEN_FILE:
+					SDL_Log("Opening file: %s", tinyfd_openFileDialog("Select file", NULL, NULL, NULL, NULL, 0));
+					break;
+				case Window::ID_DEBUG_PAINT:
+					if(image)
+						SDL_BlitSurface(image, NULL, screenSurface, NULL);
+					SDL_UpdateWindowSurface(sdlwindow);
+					break;
+				case Window::ID_DEBUG_FILL_BLACK:
+					//Fill the surface with black
+					if (screenSurface)
+						SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+					SDL_UpdateWindowSurface(sdlwindow);
+					break;
+				}
+				break;
+			case WM_PAINT:
+				/*
+				t2 = std::chrono::high_resolution_clock::now();
+
+				std::chrono::duration<double, std::milli> ms_double = t2 - t1;
+				SDL_Log("Duration between redraw: %f", ms_double.count());
+
+				t1 = t2;
+				*/
+				break;
+			default: break;
+			}
+			break;
+		case SDL_RENDER_TARGETS_RESET:
+			SDL_Log("Reset render targets");
 			break;
 		case SDL_QUIT:
 			isRunning = false;
@@ -83,44 +123,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	//Destroy window
-	SDL_DestroyWindow(window);
-
+	SDL_DestroyWindow(sdlwindow);
 	SDL_Quit();
 	return 0;
-	
-}
-
-
-//Function which retrieves the address/Handle of an SDL window
-//Also retrieves the specific subsystem used by SDL to create that window which is platform specific (Windows, MAC OS x, IOS, etc...)
-HWND getSDLWinHandle(SDL_Window* win)
-{
-	SDL_SysWMinfo infoWindow;
-	SDL_VERSION(&infoWindow.version);
-	if (!SDL_GetWindowWMInfo(win, &infoWindow))
-	{
-		return NULL;
-	}
-	return (infoWindow.info.win.window);
-}
-
-//Initializes the native windows drop down menu elements of the window
-void ActivateMenu(HWND windowRef)
-{
-	hMenuBar = CreateMenu();
-	hFile = CreateMenu();
-	hEdit = CreateMenu();
-	hHelp = CreateMenu();
-
-	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hFile, L"File");
-	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hEdit, L"Edit");
-	AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hHelp, L"Help");
-
-	
-	AppendMenu(hFile, MF_STRING, ID_TEST, L"Open");
-	AppendMenu(hFile, MF_STRING, ID_EXIT, L"Exit");
-	AppendMenu(hEdit, MF_STRING, ID_CONTROLS, L"Configure Controls");
-	AppendMenu(hHelp, MF_STRING, ID_ABOUT, L"About");
-
-	SetMenu(windowRef, hMenuBar);
 }
